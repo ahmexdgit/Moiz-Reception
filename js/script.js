3732f90a4964e438fbe19cb25547c008a115aa94
HumanDesignApp <humandesignrave@gmail.com>
2025-09-03 16:48:45 +0500

    dajkldjlkasjd

--- js/script.js
+++ js/script.js
@@ -25,44 +25,34 @@ $(document).on("click", function () {
   console.log("Shaadi me zaroor aana");
 });
 
-// Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2024 00:00:00").getTime();
+const countDownDate = new Date("Oct 3, 2025 19:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get todays date and time
-  var now = new Date().getTime();
+const countdownTimer = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Output the result in an element with id="demo"
-  document.getElementById("time").innerHTML =
-    "<div class='container'><div class='days block'>" +
-    days +
-    "<br>Days</div>" +
-    "<div class='hours block'>" +
-    hours +
-    "<br>Hours</div>" +
-    "<div class='minutes block'>" +
-    minutes +
-    "<br>Minutes</div>" +
-    "<div class='seconds block'>" +
-    seconds +
-    "<br>Seconds</div></div>";
-
-  // If the count down is over, write some text
   if (distance < 0) {
-    clearInterval(x);
+    clearInterval(countdownTimer);
     document.getElementById("time").innerHTML =
-      "Bless the married couple for happy life!";
+      "<div class='end-msg'>Bless the married couple for happy life!</div>";
+    return;
   }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("time").innerHTML = `
+      <div class="container">
+        <div class="block">${days}<span>Days</span></div>
+        <div class="block">${hours}<span>Hours</span></div>
+        <div class="block">${minutes}<span>Minutes</span></div>
+        <div class="block">${seconds}<span>Seconds</span></div>
+      </div>
+    `;
 }, 1000);
 
 // being a bit cool :p
